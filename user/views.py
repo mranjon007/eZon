eec21d280a21e6c37e0e691cad276e2e411ed533
mranjon007 <you@example.com>
2019-10-17 19:12:20 +0600

    test phone number verification added

--- user/views.py
+++ user/views.py
@@ -6,6 +6,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.views import LoginView
 from django.http import HttpResponseRedirect
 from .models import PhoneNumberVerification, CustomUser
+from django.urls import reverse
 import random
 # print(random.randint(1,101))
 
@@ -24,9 +25,8 @@ def signup(request):
             email = form.cleaned_data.get('email')
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username=email, password=raw_password)
-
-            login(request, user)
-            return H('home')
+            #login(request, user)
+            return HttpResponseRedirect(reverse('verify-phone-number', kwargs={'user_id': user.id}))
     else:
         form = CustomUserCreationForm()
     return render(request, 'registration/signup.html', {'form': form})
@@ -34,29 +34,42 @@ def signup(request):
 
 def verify_phone_number(request, user_id):
     context = {}
-    user = User.objects.get(user_id)
+    user = CustomUser.objects.filter(id=user_id).first()
     context['user'] = user
-    verification_instance = PhoneNumberVerificationForm.objects.filters(user=user)
+    verification_instance = PhoneNumberVerification.objects.filter(user=user).first()
 
-    if verification_instance.is_varified is not None:
-        context['message'] = "this phone number is not verified"
-    else:
-        if request.POST & is_verified is None:
-            form = PhoneNumberVerificationForm(request.POST)
-            if form.is_valid():
-                verification_code = form.cleaned_data['verification_code']
+    if request.method == 'GET':
+        form = PhoneNumberVerificationForm()
+        context['form'] = form
+        if verification_instance is None or verification_instance.count < 5:
+            #code = random.randint(1000, 9999)
+            code = 4444
+            verification_instance, is_created =\
+                PhoneNumberVerification.objects.get_or_create(user=user,
+                                                              )
 
-                PhoneNumberVerification.objects.create(user=user,
-                                                       verification_code=verification_code,
-                                                       )
-                return HttpResponseRedirect(reverse('login'))
-
+            verification_instance.verification_code = code
+            # is_sent = send_verificaiton_code(user.phone_number)
+            verification_instance.count += 1
+            verification_instance.save()
         else:
-            form = ProductSendToDeliveryForm()
+            context['message'] = "We sent verification code to your phone for 5 times already. Please Call eZon support for more information"
+        return render(request, template_name='user/phone_number_verification.html',
+                      context=context)
+    else:
+        form = PhoneNumberVerificationForm(request.POST)
+        if form.is_valid():
+            verification_code = form.cleaned_data.get('verification_code')
 
-            context['form'] = form
-            return render(request, template_name='user/phone_number_verification.html',
-                          context=context)
+            if str(verification_instance.verification_code) == verification_code:
+                login(request, user)
+                verification_instance.is_verified = True
+                return HttpResponseRedirect(reverse('user-dashboard', kwargs={'user_id': user.id}))
+            else:
+                context["message"] = "Code is not match"
+                print(context["message"])
+                return render(request, template_name='user/phone_number_verification.html',
+                              context=context)
 
 
 # def send_code(request, )
